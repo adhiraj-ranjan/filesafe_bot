a24c6c0a50da4f07344e70ddd6895cb0b47a68de
Adhiraj Ranjan <adhirajranjan10@gmail.com>
2023-09-08 18:29:16 +0000

    update 2.0

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from telegram.ext import filters, MessageHandler, CommandHandler, Application, ConversationHandler, CallbackQueryHandler
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton, KeyboardButton, ReplyKeyboardMarkup
 import utils
+from warnings import filterwarnings
+from telegram.warnings import PTBUserWarning
+
+filterwarnings(action="ignore", message=r".*CallbackQueryHandler", category=PTBUserWarning)
 
 TOKEN = "6422247925:AAHQ-TGNUfFXG1MsaJGMAAfbn0vuaDmw5D4"
 CHANNEL_CHAT_ID = -1001982682215
@@ -8,12 +12,13 @@ CHANNEL_CHAT_ID = -1001982682215
 CHANNEL_USERNAME = "@truebotsc"
 
 # Define conversation states
+
 USER_PHRASE_SET, USER_PASSWORD_SET = range(2)
 USER_PHRASE_GET, USER_PASSWORD_GET = range(2)
 
 async def start(update, context):
-    p = KeyboardButton(text="Retrieve File")
-    await update.message.reply_text("Send any file, and specify its a secret phrase and password, for the file, and access the file anytime later.", reply_markup=ReplyKeyboardMarkup([[p]]))
+    await update.message.reply_text("Send any file, and specify a secret phrase and password for the file, and access the file anytime later.",
+                                     reply_markup=ReplyKeyboardMarkup([[KeyboardButton(text="Retrieve File")]], resize_keyboard=True))
 
 
 async def file_received(update, context):
@@ -37,13 +42,13 @@ async def file_received(update, context):
     #await update.message.forward(CHANNEL_CHAT_ID)
 
 async def random_text(update, context):
-    await update.message.reply_text("not found")
+    await update.message.reply_text("unknown command")
 
 
 async def phrase_handler(update, context):
     query = update.callback_query
     context.user_data['ids'] = [query.data, query.message.message_id]
-    await query.message.reply_text("→ ENTER A PHRASE TO REMEMBER THE FILE ←")
+    await query.message.reply_text("→ ENTER A PHRASE TO REMEMBER THE FILE ←", reply_markup=ReplyKeyboardMarkup([[KeyboardButton(text="/cancel")]], resize_keyboard=True))
     return USER_PHRASE_SET
 
 
@@ -57,7 +62,7 @@ async def handle_password(update, context):
     f_msg = await context.bot.forward_message(CHANNEL_CHAT_ID, update.message.chat.id, context.user_data['ids'][0])
     utils.add_file(f"{context.user_data['phrase']}:{update.message.text}", f_msg.message_id)
 
-    await update.message.reply_text("File Saved !", reply_to_message_id=context.user_data['ids'][0])
+    await update.message.reply_text("File Saved !", reply_to_message_id=context.user_data['ids'][0], reply_markup=ReplyKeyboardMarkup([[KeyboardButton(text="Retrieve File")]], resize_keyboard=True))
     
     # End the conversation
     context.user_data.clear()
@@ -65,7 +70,9 @@ async def handle_password(update, context):
 
 
 async def get_handler(update, context):
-    await update.message.reply_text("→ ENTER THE PHRASE OF THE FILE ←")
+    await update.message.reply_text("→ ENTER THE PHRASE OF THE FILE ←",
+                                     reply_markup=ReplyKeyboardMarkup([[KeyboardButton(text="/cancel")]],
+                                     resize_keyboard=True))
     return USER_PHRASE_GET
 
 
@@ -76,22 +83,31 @@ async def get_phrase(update, context):
 
 async def get_password(update, context):
     
+    await update.message.reply_text("Checking... ✅", reply_markup=ReplyKeyboardMarkup([[KeyboardButton(text="Retrieve File")]], resize_keyboard=True))
     req_file_id = utils.get_message_id(f"{context.user_data['phrase']}:{update.message.text}")
     if req_file_id:
         await context.bot.forward_message(update.message.chat.id, CHANNEL_CHAT_ID, req_file_id)
     else:
-        await update.message.reply_text("FILE_NOT_FOUND_ERROR - phrase or password is incorrect")
+        await update.message.reply_text("FILE_NOT_FOUND ❌")
     
     # End the conversation
     context.user_data.clear()
+
+    return ConversationHandler.END
+
+async def end_convo(update, context):
+    context.user_data.clear()
+
+    await update.message.reply_text("Process Cancelled ✅", reply_markup=ReplyKeyboardMarkup([[KeyboardButton(text="Retrieve File")]], resize_keyboard=True))
+    
     return ConversationHandler.END
 
 
 # Log errors
 async def error(update, context):
-    if update:
-        await update.message.reply_text(str(context.error))
-        print(context.error)
+    #if update:
+    await update.message.reply_text(str(context.error))
+    print(context.error)
 
 
 # Run the program
@@ -100,27 +116,27 @@ if __name__ == '__main__':
 
     # Commands
     app.add_handler(CommandHandler('start', start))
+
     app.add_handler(MessageHandler(filters.Document.ALL | filters.PHOTO | filters.AUDIO | filters.VIDEO, file_received))
-    
+
     # Create a ConversationHandler to manage the flow
     set_conv_handler = ConversationHandler(
-        entry_points=[CallbackQueryHandler(phrase_handler)],
+        entry_points=[CallbackQueryHandler(phrase_handler, pattern="^\d+$")],
         states={
-            USER_PHRASE_SET: [MessageHandler(filters.TEXT, handle_phrase)],
-            USER_PASSWORD_SET: [MessageHandler(filters.TEXT, handle_password)],
+            USER_PHRASE_SET: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_phrase)],
+            USER_PASSWORD_SET: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_password)],
         },
-        fallbacks=[],
+        fallbacks=[CommandHandler('cancel', end_convo)],
     )
 
     get_conv_handler = ConversationHandler(
         entry_points=[MessageHandler(filters.Regex("Retrieve File"), get_handler)],
         states={
-            USER_PHRASE_GET: [MessageHandler(filters.TEXT, get_phrase)],
-            USER_PASSWORD_GET: [MessageHandler(filters.TEXT, get_password)],
+            USER_PHRASE_GET: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_phrase)],
+            USER_PASSWORD_GET: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_password)],
         },
-        fallbacks=[],
+        fallbacks=[CommandHandler('cancel', end_convo)],
     )
-
     # Add the ConversationHandler to the dispatcher
     app.add_handler(set_conv_handler)
     app.add_handler(get_conv_handler)
